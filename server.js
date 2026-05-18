aee6bd4b9f25f59397c36bc48a4a9d6f4f890daf
keokistevenson <keokistevenson@gmail.com>
2026-05-18 19:18:47 -0400

    made configuration changes and did first draft of server.js file.

---
+++ server.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+
+
+
+// MIDDLEWARE
+
+// ROUTES
+app.get("/", (req, res)=>{
+    res.send("Server is connected.");
+})
+
+// PORT
+app.listen(port, (req, res)=>{
+    console.log(`Server is connect on port http://localhost:${port}.`);
+})
