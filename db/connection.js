715d61127e29305320ccfef02e45f26af059e318
keokistevenson <keokistevenson@gmail.com>
2026-05-18 20:33:53 -0400

    Daft a db connection.

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+// DEPENDENCIES
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// DATABASE
+mongoose.connect(process.env.DATABASE_URI);
+
+const db = mongoose.connection();
+db.on("error", (err)=> console.log(err.message + " is mongo not running?"));
+db.on("connected", ()=> console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected."));
+
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-
 const port = process.env.PORT;
 
 
