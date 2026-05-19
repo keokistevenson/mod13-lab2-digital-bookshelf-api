452d5f0afc86c83751604564098b69c34c3136a8
keokistevenson <keokistevenson@gmail.com>
2026-05-18 22:15:17 -0400

    Stubbed out bookRoutes.js

---
+++ routes/bookRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+// Create: POST / - Creates a new book using the data in req.body.
+router.post("/", ()=> {
+
+});
+
+// Read All: GET / - Retrieves all books from the database.
+router.get("/", ()=> {
+
+});
+
+// Read One: GET /:id - Retrieves a single book by its _id.
+router.get("/", ()=> {
+
+});
+
+// Update: PUT /:id - Updates a book by its _id using the data in req.body.
+router.put("/", ()=> {
+
+});
+
+// Delete: DELETE /:id - Deletes a book by its _id.
+router.delete("/", ()=> {
+
+});
